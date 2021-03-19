d663ba8f04b429fdbb331739ef9487aae9f54a66
mailson <bsi.mailson@gmail.com>
2021-03-19 01:03:41 -0400

    finalizando uma pledge

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,17 @@
+// datas situation inicial
+const totalAmountPromises = document.querySelector('.total-amount-promises');
+const totalBackers = document.querySelector('.total-backers');
+const leftDays = document.querySelector('.days');
+
+const pBar = document.querySelector('.bar-fill');
+
 // modal-start-selectors
 const btnOpenModal = document.querySelectorAll('[data-modal-open]');
 const btnCloseModal = document.querySelectorAll('[data-modal-close]');
-const thankyouModal = document.querySelector('.modal-gratefulness');
 const overlayModal = document.getElementById('overlay-modal');
-const pledgeModal = document.querySelectorAll('.modal-start .pledges')
+const modalPledgeView = document.querySelectorAll('.modal-start .pledges');
+const modalStart = document.querySelector('#modal-start');
+const thankyouModal = document.querySelector('#modal-gratefulness');
 
 
 //buttons-selectors
@@ -11,39 +19,81 @@ const btnBookmark = document.querySelector('.button.bookmark');
 const bookmarkImg = document.querySelector('#bookmark-img');
 const bookmarkSpan = document.querySelector('#bookmark-span');
 
+//forms-selector
+const standForm = document.querySelector('#bamboo-stand-form');
+const standFormInput = document.querySelector('#bamboo-stand-form input');
+
 
 // "database-situation"
-const masterCraftDB ={
+const masterCraftDB = {
     initialStatus:{
-        amauntBacked: 89914,
+        amountPromises: 89914,
         goal: 100000,
         backers: 5007,
         daysLeft: 56
-    }
-
+    },
+    views:
+    [
+        {
+            name: 'Bamboo Stand',
+            reward: 101,
+            minPledge: 25,
+            display: document.querySelectorAll('.inventory'),
+            container: document.querySelectorAll('.bamboo')
+        },
+        {
+            name: 'Black Edition',
+            reward: 64,
+            minPledge: 75,
+            display: document.querySelectorAll('.inventory'),
+            container: document.querySelectorAll('.black-edition')
+        },
+        {
+            name: 'Black Edition',
+            reward: 0,
+            minPledge: 200,
+            display: document.querySelectorAll('.inventory'),
+            container: document.querySelectorAll('.black-edition')
+        }
+    ]
 }
+
+//Pledges forms
+standForm.addEventListener('submit', (evt)=>{
+    const pledgeOfAmount = parseInt(standFormInput.value);
+    calcDB(pledgeOfAmount,0)
+    updateDataSituation()
+    updateAmountDisplay()
+    closeModals(modalStart)
+    openModals(thankyouModal)
+    soldOff()
+    updateProgressBar()
+    evt.preventDefault()   
+    
+})
+
+
+updateDataSituation()
+updateAmountDisplay()
+updateProgressBar()
 
 
 // Open-Close modal
 btnOpenModal.forEach( btnClicked =>{
     btnClicked.addEventListener('click', ()=>{
-        
-        if(btnClicked.dataset.pledge){
-            const modal = document.querySelector(btnClicked.dataset.modalOpen);
+        const modal = document.querySelector(btnClicked.dataset.modalOpen);        
+        if(btnClicked.dataset.pledge){            
             openModals(modal);
-
             if( btnClicked.dataset.pledge == '1'){// If you choose a pledge external, 
-                                                 //capture the index of the element 
-                                                 //from within the modal-start.
-                openPreviouslyChosenPledges(3);
+                                            //capture the index of the element from within the modal-start.
+                openPreviouslyChosenPledges(3, modal);
             }else if(btnClicked.dataset.pledge == '2'){
-                openPreviouslyChosenPledges(4);
+                openPreviouslyChosenPledges(4, modal);
             }else if(btnClicked.dataset.pledge == '3'){
-                openPreviouslyChosenPledges(5);
+                openPreviouslyChosenPledges(5, modal);
             }
 
         }else{
-            const modal = document.querySelector(btnClicked.dataset.modalOpen);
             openModals(modal);
         }
         
@@ -53,8 +103,10 @@ btnOpenModal.forEach( btnClicked =>{
 })
 
 btnCloseModal.forEach(btnClicked =>{
+    
     btnClicked.addEventListener('click', ()=>{
-        const modal = btnClicked.closest('.modal-start')
+       
+        const modal = btnClicked.closest('.modal-start')        
         closeModals(modal)
     })
 })
@@ -68,16 +120,25 @@ function openModals(modal){
 }
 
 function closeModals(modal){
+    
     if(modal == null) return
-    modal.classList.remove('show')
-    overlayModal.classList.remove('show')
-    uncheckRadioBtn()
-    hideFormers()    
+    //{
+    //     const modal_start = document.querySelector("#modal-start")
+    //     modal_start.classList.remove('show')
+    //     overlayModal.classList.remove('show')
+    // }else{
+        modal.classList.remove('show')
+        overlayModal.classList.remove('show')
+        uncheckRadioBtn()
+        hideFormers() 
+    // }     
 }
 
 
+
+
 // Show-hide footer pledges from the modal pledges
-pledgeModal.forEach(pledgeChecked =>{
+modalPledgeView.forEach(pledgeChecked =>{
     pledgeChecked.addEventListener('click', ()=>{
         checkRadioBtn(pledgeChecked); //Even if the click is on the pledge, 
                                       //the radio button is marked.      
@@ -88,9 +149,10 @@ pledgeModal.forEach(pledgeChecked =>{
 
 
 // Show-hide footer pledges from the external pledges
-function openPreviouslyChosenPledges(chosenPledge){
-    let pledge = document.querySelector("#modal-start").children[chosenPledge];
-    let footer = pledge.querySelector('.footer');
+function openPreviouslyChosenPledges(chosenPledge, modal){
+    //const pledge = document.querySelector("#modal-start").children[chosenPledge];
+    const pledge = modal.children[chosenPledge];    
+    const footer = pledge.querySelector('.footer');    
     checkRadioBtn(pledge);
     toggleForm(pledge, footer);
 }
@@ -137,4 +199,46 @@ btnBookmark.addEventListener('click', ()=>{
     }
 
 })
+
+//updates the db values
+function calcDB(amount, view){     
+    masterCraftDB.initialStatus.amountPromises += amount; //sum the promised amount;    
+    masterCraftDB.initialStatus.backers ++ //sum one more backer;
+    if(view >= 0){
+        masterCraftDB.views[view].reward-- //subtracts a reward.
+    }
+}
+
+function updateDataSituation(){    
+    totalAmountPromises.textContent = `$${masterCraftDB.initialStatus.amountPromises}`;
+    totalBackers.textContent = `${masterCraftDB.initialStatus.backers}`;
+    leftDays.textContent = `${masterCraftDB.initialStatus.daysLeft}`;
+
+}
+
+function updateAmountDisplay(){
+    masterCraftDB.views.forEach(view =>{
+        view.display.forEach(display =>{
+            display.innerHTML = `${view.reward}`
+        })
+    })
+}
+
+function soldOff(){
+    masterCraftDB.views.forEach(view =>{
+        if(view.reward === 0){
+            view.container.forEach(container =>{
+                container.classList.add('sold-off');
+                const button = container.querySelector('button');
+                button.textContent = 'Sold off'
+            })
+        }
+    })
+}
+
+function updateProgressBar(){
+    const pb = (masterCraftDB.initialStatus.amountPromises/masterCraftDB.initialStatus.goal)*100;
+    pBar.style.width = `${pb}%`;
+
+}
 
